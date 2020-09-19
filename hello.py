f915a29632f1150b1bd93530e979d49122a2e8a5
RITESH KR <27215092+RITZ29@users.noreply.github.com>
2020-09-19 16:31:53 +0530

    init

--- hello.py
+++ hello.py
@@ -1,9 +1,1 @@
 print("Hello World")
-
-print("Checking for new changes in git")
-
-print("Checked and all are working")
-
-print("Cant Directly commit through vscode")
-
-print("Finally can commit directly from vscode")
